981a95f06435760ed555177b772d31c338585268
‘deng’ <1253850932@qq.com>
2023-06-23 21:22:22 +0800

    feat: 项目初始化

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import './style.css'
+import './assets/reset.css'
+import App from './App.vue'
+import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(router).use(ElementPlus).mount('#app')
---
+++ src/pages/login.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="login">
+    <el-form label-width="100px" style="max-width: 460px" class="login-container">
+      <el-form-item label="账号">
+        <el-input />
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input />
+      </el-form-item>
+      <el-form-item>
+        <el-button>登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<style scoped lang="scss">
+.login {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
